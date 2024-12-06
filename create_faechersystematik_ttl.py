17777e5c9cb2d42e9b9622c4963c632e583600fb
Petra Maier <petra.maier@hbz-nrw.de>
2024-12-06 10:03:47 +0100

    Add concept 128 with old uri (#41)

--- create_faechersystematik_ttl.py
+++ create_faechersystematik_ttl.py
@@ -3,6 +3,7 @@ import rdflib.term
 from rdflib import Graph, Literal, RDF, URIRef, Namespace, DCTERMS
 import logging
 
+
 def extract_preflabel_translations(current_ttl):
     pref_label_dict_list = []
     deprecated_broader_list = []
@@ -54,7 +55,7 @@ def extract_deprecated_notations(current_ttl):
 
 def add_pref_labels_lang(level_dict_list, current_pref_labels_dict):
     for idx, i in enumerate(level_dict_list):
-        notation = level_dict_list[idx]['notation']
+        notation = level_dict_list[idx]['uri']
         if notation in current_pref_labels_dict:
             for k,v in current_pref_labels_dict.items():
                 pref_label_en = v.get("label_en")
@@ -67,9 +68,9 @@ def extract_narrower(dict_list):
     for dict in dict_list:
         notation_broader_level = dict["broader"]
         if notation_broader_level in narrower_dict:
-            narrower_dict[notation_broader_level].append(dict["notation"])
+            narrower_dict[notation_broader_level].append(dict["uri"])
         else:
-            narrower_dict[notation_broader_level]=[dict["notation"]]
+            narrower_dict[notation_broader_level]=[dict["uri"]]
     return narrower_dict
 
 def add_narrower(dict_list_broader, dict_list_narrower, deprecated_broader_list):
@@ -79,9 +80,11 @@ def add_narrower(dict_list_broader, dict_list_narrower, deprecated_broader_list)
             if k in narrower:
                 narrower[k].append(v)
     for d in dict_list_broader:
-        if d["notation"] in narrower:
-            d.update(narrower=narrower[d["notation"]])
+        if d["uri"] in narrower:
+            d.update(narrower=narrower[d["uri"]])
 
+def copy_notation(dictionary):
+    dictionary["uri"] = dictionary["notation"]
 
 # extract translations of prefLabels
 current_hfs_file = "https://github.com/dini-ag-kim/hochschulfaechersystematik/blob/master/hochschulfaechersystematik.ttl?raw=true"
@@ -107,6 +110,14 @@ df_3rd_level = df_3rd_level[df_3rd_level.broader !="83"]
 
 df_1st_level['notation'] = df_1st_level['notation'].str.lstrip("0")
 df_2nd_level['broader'] = df_2nd_level['broader'].str.lstrip("0")
+
+# add new column "uri" based on "notation" to enable the URI to be assigned independently of the notations
+copy_notation(df_1st_level)
+copy_notation(df_2nd_level)
+copy_notation(df_3rd_level)
+
+# change uri "128" > "0128"
+df_3rd_level["uri"] = df_3rd_level["uri"].replace("128", "0128")
 
 dict_1st_level = df_1st_level.to_dict("records")
 dict_2nd_level = df_2nd_level.to_dict("records")
@@ -140,14 +151,16 @@ g.add((URIRef('scheme'), dct['title'], Literal('Destatis-Systematik der Fächerg
 g.add((URIRef('scheme'), dct['alternative'], Literal('Hochschulfächersystematik', lang='de')))
 g.add((URIRef('scheme'), dct['description'], Literal('Diese SKOS-Klassifikation basiert auf der Destatis-[\"Systematik der Fächergruppen, Studienbereiche und Studienfächer\"](https://bartoc.org/en/node/18919).', lang='de')))
 g.add((URIRef('scheme'), dct['issued'], Literal('2019-12-11')))
+g.add((URIRef('scheme'), dct['license'], rdflib.term.URIRef('http://dcat-ap.de/def/licenses/other-closed')))
 g.add((URIRef('scheme'), dct['publisher'], rdflib.term.URIRef('https://oerworldmap.org/resource/urn:uuid:fd06253e-fe67-4910-b923-51db9d27e59f')))
+g.add((URIRef('scheme'), dct['source'], rdflib.term.URIRef('https://www.destatis.de/DE/Methoden/Klassifikationen/Bildung/studenten-pruefungsstatistik.html')))
 g.add((URIRef('scheme'), vann['preferredNamespaceUri'], Literal('https://w3id.org/kim/hochschulfaechersystematik/')))
 g.add((URIRef('scheme'), vann['preferredNamespacePrefix'], Literal('hfs')))
 g.add((URIRef('scheme'), schema['isBasedOn'], rdflib.term.URIRef('http://bartoc.org/node/18919')))
 
 
 for idx, i in enumerate(dict_1st_level):
-    top_level = dict_1st_level[idx]['notation']
+    top_level = dict_1st_level[idx]['uri']
     g.add((URIRef('n%s' % top_level), RDF['type'], skos['Concept']))
     g.add((URIRef('n%s' % top_level), skos['topConceptOf'], (URIRef('scheme'))))
     g.add((URIRef('n%s' % top_level), skos['prefLabel'], Literal(dict_1st_level[idx]['label'], lang='de')))
@@ -156,38 +169,38 @@ for idx, i in enumerate(dict_1st_level):
     if dict_1st_level[idx].get('label_en'):
         g.add((URIRef('n%s' % top_level), skos['prefLabel'], Literal(dict_1st_level[idx]['label_en'], lang='en')))
         g.add((URIRef('n%s' % top_level), skos['prefLabel'], Literal(dict_1st_level[idx]['label_uk'], lang='uk')))
-        g.add((URIRef('n%s' % top_level), skos['notation'], Literal(top_level)))
+        g.add((URIRef('n%s' % top_level), skos['notation'], Literal(dict_1st_level[idx]['notation'])))
     else:
         logging.warning("No translation for {notation}".format(notation=top_level))
     g.add((URIRef('scheme'), skos['hasTopConcept'], (URIRef('n%s' % top_level))))
-    for idx_2, i_2 in enumerate(dict_2nd_level):
-        if dict_2nd_level[idx_2]['broader'] == top_level:
-            level_2_notation = dict_2nd_level[idx_2]['notation']
-            g.add((URIRef('n%s' % level_2_notation), RDF['type'], skos['Concept']))
-            g.add((URIRef('n%s' % level_2_notation), skos['prefLabel'], Literal(dict_2nd_level[idx_2]['label'], lang='de')))
-            if dict_2nd_level[idx_2].get('label_en'):
-                g.add((URIRef('n%s' % level_2_notation), skos['prefLabel'], Literal(dict_2nd_level[idx_2]['label_en'], lang='en')))
-                g.add((URIRef('n%s' % level_2_notation), skos['prefLabel'], Literal(dict_2nd_level[idx_2]['label_uk'], lang='uk')))
-            else:
-                logging.warning("No translation for {notation}".format(notation=level_2_notation))
-            g.add((URIRef('n%s' % level_2_notation), skos['broader'], (URIRef('n%s' % dict_2nd_level[idx_2]['broader']))))
-            for item in dict_2nd_level[idx_2]['narrower']:
-                g.add((URIRef('n%s' % level_2_notation), skos['narrower'], (URIRef('n%s' % item))))
-            g.add((URIRef('n%s' % level_2_notation), skos['notation'], Literal(level_2_notation)))
-            g.add((URIRef('n%s' % level_2_notation), skos['inScheme'], (URIRef('scheme'))))
-            for idx_3, i_3 in enumerate(dict_3rd_level):
-                if dict_3rd_level[idx_3]['broader'] == level_2_notation:
-                    level_3_notation = dict_3rd_level[idx_3]['notation']
-                    g.add((URIRef('n%s' % level_3_notation), RDF['type'], skos['Concept']))
-                    g.add((URIRef('n%s' % level_3_notation), skos['prefLabel'],Literal(dict_3rd_level[idx_3]['label'], lang='de')))
-                    if dict_3rd_level[idx_3].get('label_en'):
-                        g.add((URIRef('n%s' % level_3_notation), skos['prefLabel'],Literal(dict_3rd_level[idx_3]['label_en'], lang='en')))
-                        g.add((URIRef('n%s' % level_3_notation), skos['prefLabel'],Literal(dict_3rd_level[idx_3]['label_uk'], lang='uk')))
-                    else:
-                        logging.warning("No translation for {notation}".format(notation=level_3_notation))
-                    g.add((URIRef('n%s' % level_3_notation), skos['notation'], Literal(level_3_notation)))
-                    g.add((URIRef('n%s' % level_3_notation), skos['inScheme'], (URIRef('scheme'))))
-                    g.add((URIRef('n%s' % level_3_notation), skos['broader'], (URIRef('n%s' % dict_3rd_level[idx_3]['broader']))))
+
+for idx_2, i_2 in enumerate(dict_2nd_level):
+    level_2_uri = dict_2nd_level[idx_2]['uri']
+    g.add((URIRef('n%s' % level_2_uri), RDF['type'], skos['Concept']))
+    g.add((URIRef('n%s' % level_2_uri), skos['prefLabel'], Literal(dict_2nd_level[idx_2]['label'], lang='de')))
+    if dict_2nd_level[idx_2].get('label_en'):
+        g.add((URIRef('n%s' % level_2_uri), skos['prefLabel'], Literal(dict_2nd_level[idx_2]['label_en'], lang='en')))
+        g.add((URIRef('n%s' % level_2_uri), skos['prefLabel'], Literal(dict_2nd_level[idx_2]['label_uk'], lang='uk')))
+    else:
+        logging.warning("No translation for {notation}".format(notation=level_2_uri))
+    g.add((URIRef('n%s' % level_2_uri), skos['broader'], (URIRef('n%s' % dict_2nd_level[idx_2]['broader']))))
+    for item in dict_2nd_level[idx_2]['narrower']:
+        g.add((URIRef('n%s' % level_2_uri), skos['narrower'], (URIRef('n%s' % item))))
+    g.add((URIRef('n%s' % level_2_uri), skos['notation'], Literal(dict_2nd_level[idx_2]['notation'])))
+    g.add((URIRef('n%s' % level_2_uri), skos['inScheme'], (URIRef('scheme'))))
+
+for idx_3, i_3 in enumerate(dict_3rd_level):
+    level_3_uri = dict_3rd_level[idx_3]['uri']
+    g.add((URIRef('n%s' % level_3_uri), RDF['type'], skos['Concept']))
+    g.add((URIRef('n%s' % level_3_uri), skos['prefLabel'], Literal(dict_3rd_level[idx_3]['label'], lang='de')))
+    if dict_3rd_level[idx_3].get('label_en'):
+        g.add((URIRef('n%s' % level_3_uri), skos['prefLabel'], Literal(dict_3rd_level[idx_3]['label_en'], lang='en')))
+        g.add((URIRef('n%s' % level_3_uri), skos['prefLabel'], Literal(dict_3rd_level[idx_3]['label_uk'], lang='uk')))
+    else:
+        logging.warning("No translation for {notation}".format(notation=level_3_uri))
+    g.add((URIRef('n%s' % level_3_uri), skos['notation'], Literal(dict_3rd_level[idx_3]['notation'])))
+    g.add((URIRef('n%s' % level_3_uri), skos['inScheme'], (URIRef('scheme'))))
+    g.add((URIRef('n%s' % level_3_uri), skos['broader'], (URIRef('n%s' % dict_3rd_level[idx_3]['broader']))))
 
 g.add((URIRef('n0'), RDF['type'], skos['Concept']))
 g.add((URIRef('n0'), skos['prefLabel'], Literal('Fachübergreifend', lang='de')))
